4b567a1e88412f2876794384c8f39f117de7cbd6
ChandraTripathi <82561011+ChandraTripathi@users.noreply.github.com>
2021-04-26 12:16:40 +0530

    Add files via upload
    challange functions

---
+++ function prime or not using recursion .c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int checkForPrime(int);
+int i;
+
+int main()
+{
+
+    int n1,primeNo;
+
+	printf("\n\n Recursion : Check a number is prime number or not :\n");
+	printf("--------------------------------------------------------\n");
+	
+    printf(" Input any positive number : ");
+    scanf("%d",&n1);
+    
+    i = n1/2;
+
+    primeNo = checkForPrime(n1);//call the function checkForPrime
+
+   if(primeNo==1)
+        printf(" The number %d is a prime number. \n\n",n1);
+   else
+      printf(" The number %d is not a prime number. \nn",n1);
+   return 0;
+}
+
+int checkForPrime(int n1)
+{
+    if(i==1)
+    {
+        return 1;
+    }
+    else if(n1 %i==0)
+    {
+         return 0;
+    }     
+    else
+       {
+         i = i -1; 
+         checkForPrime(n1);//calling the function checkForPrime itself recursively
+      }
+}
